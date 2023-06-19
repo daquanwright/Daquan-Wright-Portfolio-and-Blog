beb2b03eadcad746e173f2fe253c697aa00b9009
Daquan Wright <daquanewright@gmail.com>
2023-06-19 11:23:36 -0400

    push up portfolio site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './index.css';
+import './responsive.css';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Blog from './components/Blog';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <div className='App'>
+          <Navigation/>
+
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/Portfolio' element={<Portfolio/>} />
+            <Route path='/Blog' element={<Blog />} />
+            <Route path='/About' element={<About/>} />
+            <Route path='/Contact' element={<Contact/>} />
+          </Routes>
+          
+          <Footer/>
+        </div>
+    );
+  }
 }
 
 export default App;
